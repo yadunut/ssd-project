4f9dc21a17901963d5b113d2e2b3e56b876ea2cd
Yadunand Prem <yadunut@gmail.com>
2018-07-12 16:15:37 +0800

    Add fields during registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,20 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_many :posts, dependent: :destroy
+
+  validates :first_name, presence: true
+  validate :validate_age
+
+  private
+
+  def validate_age
+    return false if date_of_birth.present? && date_of_birth < 18.years.ago
+    errors.add(:date_of_birth, 'You should be over 18 years old.')
+  end
 end
