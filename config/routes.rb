2d7c9eb259d95a2755e3807b545c5f33e3a88861
Yadunand Prem <yadunut@gmail.com>
2018-07-24 22:55:19 +0800

    Add nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :posts, only: %i[index create destroy]
   root 'posts#index'
 
-  get 'users/search/:id', to: 'users/users#search', as: :users_search
+  get 'users/search', to: 'users/users#search', as: :users_search
 
   get 'users/profile/:username', to: 'users/users#profile', as: :users_profile
   get 'users/profile', to: 'users/users#slashprofile'
