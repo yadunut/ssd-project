c1934ef42c662dffc072ce135078cad884788527
Yadunand Prem <yadunut@gmail.com>
2018-07-25 17:06:53 +0800

    Add admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,20 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # Redirect hook
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      puts "Resource is Admin"
+      admin_root_path_url
+    when User
+      puts "Resource is User"
+      super
+    else
+      super
+    end
+  end
 end
 
 # Extension function to check if it is a string
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,42 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+  devise_for :admins
+
   resources :posts, only: %i[index create destroy]
-  root 'posts#index'
 
+  # Only admins can do
+  authenticated :admin do
+    root 'admins/admins#index'
+    get 'admins', to: 'admins/admins#index', as: :admin_root_path
+  end
+
+  # Only users can do
+  authenticated :user do
+    root 'posts#index'
+
+    get 'users/blocks', to: 'users/blocks#index', as: :block
+    get 'users/blocks/new', to: 'users/blocks#new', as: :new_block
+    post 'users/blocks', to: 'users/blocks#create', as: :create_block
+    delete 'users/blocks/:id', to: 'users/blocks#destroy', as: :destroy_block
+
+    post 'users/profile', to: 'users/users#update', as: :update_users_profile
+  end
+
+  # Non signed in users
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
+  # Everyone
   get 'users/search', to: 'users/users#search', as: :users_search
-
   get 'users/profile/:username', to: 'users/users#profile', as: :users_profile
   get 'users/profile', to: 'users/users#slashprofile'
-  post 'users/profile', to: 'users/users#update', as: :update_users_profile
 
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
 
-  get 'users/blocks', to: 'users/blocks#index', as: :block
 
-  get 'users/blocks/new', to: 'users/blocks#new', as: :new_block
-  post 'users/blocks', to: 'users/blocks#create', as: :create_block
-
-  delete 'users/blocks/:id', to: 'users/blocks#destroy', as: :destroy_block
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
 end
